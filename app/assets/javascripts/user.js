7b07ab6fd286c7f03a6f9c7b6e211fe69902eb14
Developer <dev@Pair2.local>
2015-11-11 12:23:54 +0800

    not finish yet with edit password

---
+++ app/assets/javascripts/user.js
@@ -0,0 +1,10 @@
+var checkTabErrors = function(){
+  var tabIds = $('.tab-pane .has-error').map(function(){
+    // find the parent tab id
+    var tabId = $(this).parents('.tab-pane').attr('id');
+    var tabNav = $("[data-toggle='tab'][href='#" + tabId + "']").addClass('errors');
+  });
+
+  // show the first tab with an error
+  $('.nav-tabs li > .errors').first().tab('show');
+};
